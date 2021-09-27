0383b86c6831404d9c4552dda885fb7245935634
deenz <67574872+danzel-py@users.noreply.github.com>
2021-09-27 12:56:41 +0700

    handle out of range guess

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+  value: i32,
+}
+
+impl Guess {
+  pub fn new(value: i32) -> Guess {
+    if value < 1 || value > 100 {
+      panic!("Guess must be between 1 and 100, got {}.", value);
+    }
+    Guess {value}
+  }
+
+  pub fn value(&self) -> i32 {
+    self.value
+  }
+}
+
 fn main() {
   println!("数を当ててごらん！");
   let secret_number = rand::thread_rng().gen_range(1..101); // 1..=100
@@ -13,10 +30,12 @@ fn main() {
       .expect("行の読み込みに失敗しました。");
 
     // let guess: u32 = guess.trim().parse().expect("無効な数字");
-    let guess: u32 = match guess.trim().parse() {
+    let guess: i32 = match guess.trim().parse() {
       Ok(num) => num,
       Err(_) => continue
     };
+
+    let guess = Guess::new(guess).value();
 
     // print!("推測は{}", guess);
     match guess.cmp(&secret_number) {
